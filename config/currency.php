d09142bc27a53f23feb8da083b8504a6a2876de2
Vladislav205 <51053326+Vladislav205@users.noreply.github.com>
2022-12-03 09:26:50 +0200

    Add files via upload

---
+++ config/currency.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "fixer"=>[
+        'api_key'=>env("FIXER_API_KEY", "P5SeG0z4raSIQ8LF9n6JhoSyq7OULlkr"),
+        'base_url'=>env("FIXER_BASE_URL", 'https://api.apilayer.com/exchangerates_data/'),
+    ],
+];
